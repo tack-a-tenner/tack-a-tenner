4042099c58d08556e7509479654687a60df64454
Tyler <hilltyler49@gmail.com>
2023-04-10 21:21:26 -0500

    changed location of add request button

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,18 +1,39 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
-import Auth from '../../utils/auth';
+import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { useQuery } from "@apollo/client";
+import AuthService from "../utils/auth";
+import { QUERY_REQUESTS } from "../utils/queries";
+import RequestList from "../components/RequestList";
+import RequestForm from "../components/RequestForm";
 
 const Header = () => {
   const logout = (event) => {
     event.preventDefault();
     Auth.logout();
   };
+  const { loading, data } = useQuery(QUERY_REQUESTS);
+  const requests = data?.requests || [];
+
+  const isAuthenticated = AuthService.loggedIn();
+
+  const [showForm, setShowForm] = useState(false);
+
+  const handleOpenForm = () => {
+    setShowForm(true);
+  };
+
+  const handleCloseForm = () => {
+    setShowForm(false);
+  };
+
+  const handleFormSubmit = (request) => {
+    console.log(request);
+  };
   return (
     <header className="bg-gray text-dark mb-4 py-3 display-flex align-center">
       <div className="display-flex justify-space-between-lg justify-center align-center text-center" id="nav-wrap">
         <Link to="/">
-          <h1 className="m-0" class="font-lobster" style={{ fontSize: '3rem' }}>
+          <h1 className="m-0" class="font-lobster" style={{ fontSize: "3rem" }}>
             Tack-A-Tenner
           </h1>
         </Link>
@@ -22,9 +43,10 @@ const Header = () => {
         <div>
           {Auth.loggedIn() ? (
             <>
-            <Link className="text-lobster" to="/request">
-              Make A Request
-            </Link>
+              <Link onClick={handleOpenForm}>
+                Make A Request
+                <RequestForm show={showForm} handleClose={handleCloseForm} handleSubmit={handleFormSubmit} />
+              </Link>
               <Link className="text-lobster" to="/me">
                 My Profile
               </Link>
